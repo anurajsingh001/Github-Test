1b989be7c69b00b1dd9492fd27703af4045c6df0
Avnik <abc@test.com>
2022-08-13 11:15:44 +0530

    Changes ready to Merge

--- Mysecondpythoon.py
+++ Mysecondpythoon.py
@@ -1,3 +1,10 @@
 ip = ['10.1.1.1', '10.1.1.2']
 for i in ip:
     print(i)
+#This is anoother change we are doing in program
+x = {
+    "SWA1.JP1": "10.70.1.1",
+    "SWA1.AU1": "10.86.1.1"
+}
+print(x["SWA1.AU1"])
+print(x["SWA1.JP1"])
